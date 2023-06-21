f5a791d7b6778ad3188134f3e40dfccfa32bdfc3
softcruiser <pedromoreira0046@gmail.com>
2023-06-21 17:30:19 +0900

    Create server side Express app

---
+++ server/src/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import cors from "cors";
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+const port = 8000;
+
+const corsOption = {
+  origin: "*"
+}
+
+app.use(cors(corsOption));
+app.use(express.json());
+
+// REST Api
+// import router from "./routes";
+// app.use('/api', router);
+
+// GraphQL
+// import schema from "./graphql";
+// app.use('/graphql', graphqlHTTP({
+//   schema,
+//   graphiql: true,
+// }));
+
+app.listen(port, () => {
+  console.log("=========================================================================");
+  console.log(`|                    LITHIUM SERVER STARTED AT PORT: ${port}            |`);
+  console.log("|                                                                       |");
+  console.log("|                                                created by softcruiser |");
+  console.log("=========================================================================");
+});
